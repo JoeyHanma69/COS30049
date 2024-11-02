1efdf0c259546d12159b1ca3e3e5914514078b57
Joseph Linao <linaojoseph3111@gmail.com>
2024-11-02 11:11:59 +1100

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';  
-import ReactDom from 'react-dom'; 
-import App from './App.jsx'; 
+import ReactDom from 'react-dom/client'; 
+import App from './App.jsx';  
+import './index.css'
 
 ReactDom.render( 
     <react.StrictMode> 
