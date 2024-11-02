8e29b42f8232947723ef41dbd7e9ec24106e3b43
Joseph Linao <linaojoseph3111@gmail.com>
2024-11-02 11:30:29 +1100

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';  
-import ReactDom from 'react-dom/client'; 
+import ReactDOM from 'react-dom/client'; 
 import App from './App.jsx';  
 import './index.css'
-
-ReactDom.render( 
-    <react.StrictMode> 
-        <App/>
-    </react.StrictMode>, 
-    document.getElementsById('root')
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( 
+    <React.StrictMode> 
+        <App />
+    </React.StrictMode>
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
+export default {
   server: {
-    port: 8000,  // Or any other available port
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
-});
-
+};
 
