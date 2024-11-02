2d5250729092e16bf24dc6aa89067329bad285df
Joseph Linao <linaojoseph3111@gmail.com>
2024-11-02 21:13:22 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig , loadEnv } from 'vite';
-import react from '@vitejs/plugin-react'; 
+import { defineConfig , loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 
 
@@ -9,7 +9,7 @@ export default defineConfig(({mode}) => {
   server: {
     proxy: {
       '/api': {
-        target: env.VITE_API_URL ||'http://127.0.0.1:8000',
+        target: env.VITE_API_URL || 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
