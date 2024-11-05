5b7066737f2a0569e64966bbb91a0de2cf10027d
Joseph Linao <linaojoseph3111@gmail.com>
2024-11-05 12:20:05 +1100

    zxcvc

---
+++ frontend/src/pages/EDA.jsx
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+const EDAAnalysis = () => { 
+    return ( 
+        <div className="eda-analysis-container"> 
+            <h1 className="eda-title">Exploratory Data Analysis (EDA)</h1> 
+            <p className="eda-description">Gain insghts into histocial weather trends at Melbourne Olympic Park</p> 
+            <div className="eda-content">
+                <img src="frontend\src\assets\EDAchart1.png" alt="EDA Graph" className="eda-graph" />
+                <p className="eda-summary">The EDA section shows data distributions, relationships, and trends between temperature, humidity, and weather conditions.</p>
+            </div>
+        </div>
+    );
+};
+
+export default EDAAnalysis;
